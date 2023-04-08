1418da29e2f3c10e7101615efcb56b87b3e43c87
Sujit Kumar <mnamesujit@gmail.com>
2023-04-08 22:31:35 +0530

    Opmitimized Solution

--- Step 1/Step 1.2/pattern-8.cpp
+++ Step 1/Step 1.2/pattern-8.cpp
@@ -1,38 +1,69 @@
-// URL
-// https://practice.geeksforgeeks.org/problems/triangle-pattern-1661493231/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_8
+// // URL
+// // https://practice.geeksforgeeks.org/problems/triangle-pattern-1661493231/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_8
 
+// class Solution{
+// public:
+	
+// 	void printTriangle(int n) {
+// 	    // code here
+// 	    int i=1;
+// 	    while(i<=n){
+// 	       // Printing Spaces / First Loop
+// 	        int spaces= i-1;
+// 	        while(spaces){
+// 	            cout<<" ";
+// 	            spaces--;
+// 	        }
+	        
+// 	       // Printing First Loop
+// 	       int j=1;
+// 	       while(j<=n-i+1){
+// 	           cout<<"*";
+// 	           j++;
+// 	       }
+	       
+// 	       //Printing Third Loop
+// 	        int k=n-i;
+// 	        while(k){
+// 		        cout<<"*";
+// 		        k--;
+// 	        }
+// 	       i++;
+// 	       cout<<endl;
+	       
+	        
+// 	    }
+	    
+// 	}
+// };
+
+
+
+
+// Optimized Solution
+#include <bits/stdc++.h>
+using namespace std;
 class Solution{
 public:
 	
 	void printTriangle(int n) {
 	    // code here
-	    int i=1;
-	    while(i<=n){
-	       // Printing Spaces / First Loop
-	        int spaces= i-1;
-	        while(spaces){
-	            cout<<" ";
-	            spaces--;
-	        }
-	        
-	       // Printing First Loop
-	       int j=1;
-	       while(j<=n-i+1){
-	           cout<<"*";
-	           j++;
-	       }
-	       
-	       //Printing Third Loop
-	        int k=n-i;
-	        while(k){
-		        cout<<"*";
-		        k--;
-	        }
-	       i++;
-	       cout<<endl;
-	       
-	        
-	    }
-	    
+	    for(int i=0; i<n; i++){
+            // printing Spaces
+            for(int j=0; j<i; j++){
+                cout<<" ";
+            }
+        
+            // stars
+            for( int j=0;j<2*n -(2*i +1);j++){
+                cout<<"*";
+            }
+            
+            // Spaces
+            for(int j=0; j<i; j++){
+                cout<<" ";
+            }
+            cout<<endl;
+        } 
 	}
 };
