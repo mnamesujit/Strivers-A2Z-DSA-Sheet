496135657cab822e22bbdf5dd0a9de0fd3fbc0ae
Sujit Kumar <mnamesujit@gmail.com>
2023-04-08 21:59:11 +0530

    Optimzed Solution

--- Step 1/Step 1.2/pattern-7.cpp
+++ Step 1/Step 1.2/pattern-7.cpp
@@ -1,33 +1,52 @@
 // URL
 // https://practice.geeksforgeeks.org/problems/triangle-pattern-1661492263/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_7
 
-class Solution {
-  public:
-    void printTriangle(int n) {
-        // code here
-        int i=1;
-    while(i<=n){
-    // printing first Triangle-----
-      int spaces=n-i;
-      while(spaces){
-        cout<<" ";
-        spaces--;
-      }
-    // Printing Second Triangle-------
-      int j=1;
-      while(j<=i){
-          cout<<"*";
-          j++;
+// class Solution {
+//   public:
+//     void printTriangle(int n) {
+//         // code here
+//         int i=1;
+//     while(i<=n){
+//     // printing first Triangle-----
+//       int spaces=n-i;
+//       while(spaces){
+//         cout<<" ";
+//         spaces--;
+//       }
+//     // Printing Second Triangle-------
+//       int j=1;
+//       while(j<=i){
+//           cout<<"*";
+//           j++;
           
-      }
-    // Printing Third Triangle-----
-    int k=i-1;
-    while(k){
-      cout<< "*";
-      k--;
-    }
-      cout<<endl;
-      i++;
+//       }
+//     // Printing Third Triangle-----
+//     int k=i-1;
+//     while(k){
+//       cout<< "*";
+//       k--;
+//     }
+//       cout<<endl;
+//       i++;
+//         }
+//     }
+// };
+
+// Optimized Soltion
+for(int i=0; i<n; i++){
+            // printing Spaces
+            for(int j=0; j<n-i-1; j++){
+                cout<<" ";
+            }
+        
+            // stars
+            for( int j=0;j<2*i+1;j++){
+                cout<<"*";
+            }
+            
+            // Spaces
+            for(int j=0; j<n-i-1; j++){
+                cout<<" ";
+            }
+            cout<<endl;
         }
-    }
-};
