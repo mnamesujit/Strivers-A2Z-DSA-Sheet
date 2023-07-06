9193d99361b7759d1eb8f8c80804017610a7b918
Sujit Kumar <mnamesujit@gmail.com>
2023-07-06 19:38:52 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+class Solution:
+    #Function to sort a list using quick sort algorithm.
+    def quickSort(self,arr,low,high):
+        if low < high:
+            p = self.partition(arr, low, high)
+            self.quickSort(arr, low, p)
+            self.quickSort(arr, p + 1, high)
+    
+    def partition(self,arr,low,high):
+        pivot = arr[low]
+        i = low - 1
+        j = high + 1
+        
+        while True:
+            i += 1
+            while arr[i] < pivot:
+                i += 1
+            
+            j -= 1
+            while arr[j] > pivot:
+                j -= 1
+            
+            if i>=j:
+                return j
+            
+            arr[i], arr[j] = arr[j], arr[i]
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        Solution().quickSort(arr,0,n-1)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+
+# } Driver Code Ends
