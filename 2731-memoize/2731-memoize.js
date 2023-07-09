81242e8a2a7a8acc79757710432820a80a7cbc83
Sujit Kumar <mnamesujit@gmail.com>
2023-07-09 20:31:54 +0530

    Time: 366 ms (31.12%), Space: 107.5 MB (45.84%) - LeetHub

---
+++ 2731-memoize/2731-memoize.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const cache = {}
+    return function(...args) {
+        const key=args.join('~')
+        if(cache[key] == undefined)cache[key]=fn(...args)
+        return cache[key]
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
